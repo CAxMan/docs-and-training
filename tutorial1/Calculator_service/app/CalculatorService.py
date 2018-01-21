5e0e75e301fb8ec4ee169033a1da910b9060cf11
jonhjelm <jon.hjelmervik@gmail.com>
2018-01-21 23:18:53 +0100

    added log extraction

--- tutorial1/Calculator_service/app/CalculatorService.py
+++ tutorial1/Calculator_service/app/CalculatorService.py
@@ -3,6 +3,7 @@ from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask
 from flask import request
 from flask_spyne import Spyne
+from flask import send_from_directory
 from spyne.protocol.soap import Soap11
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
@@ -16,6 +17,13 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 def hello():
     print (request.headers)
     return "Hello World http://yea"
+
+@app.route("/sintefcalc/log/<path:filename>")
+def get_log(filename):
+    print (filename)
+    return send_from_directory("/var/log/", filename, as_attachment=True)
+
+
 
 class SomeSoapService(spyne.Service):
     __service_url_path__ = '/sintefcalc/addition'
--- tutorial1/Calculator_service/app/runscript.sh
+++ tutorial1/Calculator_service/app/runscript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ -v REMOTE_HOST ]]
-then
-    cat spyne_flask.nginx_template | sed "s|REMOTE_HOST|$REMOTE_HOST|" > /etc/nginx/sites-enabled/spyne_flask.nginx
-fi
-    
+#if [[ -v REMOTE_HOST ]]
+#then
+#    cat spyne_flask.nginx_template | sed "s|REMOTE_HOST|$REMOTE_HOST|" > /etc/nginx/sites-enabled/spyne_flask.nginx
+#fi
+
+cat spyne_flask.nginx_template | sed "s|REMOTE_HOST|srv.hetcomp.org|" > /etc/nginx/sites-enabled/spyne_flask.nginx
+
 /etc/init.d/nginx start
-python CalculatorService.py
+echo here
+python CalculatorService.py |& tee -a /var/log/CalculatorService.py
 
